f02dba46371b9f59aaeb58f5f9b8ce5aa0152702
Amandeep <ads71993@gmail.com>
2017-10-28 23:59:04 -0400

    updated templates

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from users.views import CreateUser, CreateContact
 
 urlpatterns = [
     url(r'^create/', CreateUser.as_view()),
-    url(r'^contact/', CreateContact.as_view())
+    url(r'^all/', CreateContact.as_view())
     ]
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView,ListAPIView
 
 from users.models import User, Contact
 from users.serializers import UserSerializer, ContactSerializer
@@ -17,11 +17,14 @@ class CreateUser(CreateAPIView):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    renderer_classes = (TemplateHTMLRenderer,)
 
-    def get(self, request, *args, **kwargs):
-        return Response(template_name='create.html')
+class CreateUser(ListAPIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-class CreateContact(CreateAPIView):
-    queryset = Contact.objects.filter()
-    serializer_class = ContactSerializer
+# class CreateContact(CreateAPIView):
+#     queryset = Contact.objects.filter()
+#     serializer_class = ContactSerializer
