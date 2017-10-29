e7d7fd00ab2b225b676a7d45ea4fb771b2d6d4c4
Amandeep <ads71993@gmail.com>
2017-10-29 02:08:41 -0400

    updated

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 
-from users.models import User, Contact
+from users.models import User, Contact, Image
 
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('name')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password', 'image')
+        fields = ('username', 'first_name', 'last_name', 'images')
+        depth = 1
 
 
 class ContactSerializer(serializers.ModelSerializer):
