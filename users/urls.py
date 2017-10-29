f86c42b9e22631b99dae466098524fd29cb1d494
Amandeep <ads71993@gmail.com>
2017-10-29 04:47:49 -0400

    updated image tester

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from users.views import CreateUser, CreateContact
+from users.views import CreateUser, CreateContact, CheckImage
 
 urlpatterns = [
     url(r'^create/', CreateUser.as_view()),
+    url(r'^check-image/', CheckImage.as_view()),
     url(r'^all/', CreateContact.as_view())
     ]
--- users/views.py
+++ users/views.py
@@ -5,11 +5,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.generics import CreateAPIView,ListAPIView
+from rest_framework.views import APIView
 
 from users.models import User, Contact
 from users.serializers import UserSerializer, ContactSerializer
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
+
+
+class CheckImage(APIView):
+    renderer_classes = (TemplateHTMLRenderer,)
+    def get(self,request):
+        return Response(template_name='takeImage.html')
+
+    def post(self,request):
+#        do some processing
+        print request.data
+        return Response(dict(mesg=''),template_name='takeImage.html')
+
 
 class CreateUser(CreateAPIView):
     """
@@ -17,6 +30,7 @@ class CreateUser(CreateAPIView):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class ListeUser(ListAPIView):
     """
